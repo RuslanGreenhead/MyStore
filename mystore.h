06dacb2d350013274f6d31c2381c42169e1b0569
Ruslan <ruslan15-mol@mail.ru>
2022-06-06 22:53:47 +0300

    Product architecture and Position architecture added

--- mystore.h
+++ mystore.h
@@ -40,3 +40,36 @@ class AmountProduct : public Product{
 private:
     double m_cost_per_one;
 };
+
+
+//--------------------------------------All 'bout position-----------------------------------//
+
+class Position{
+public:
+    Position(Product& product) : m_ptr_product(&product){};
+    Product* get_ptr_product() { return m_ptr_product; }
+    virtual double get_cost() = 0;
+    virtual double get_quantity() = 0;
+protected:
+    Product* m_ptr_product;
+};
+
+class AmountPosition : public Position{
+public:
+    AmountPosition(AmountProduct product, size_t amount) :
+            Position(product), m_amount(amount) {};
+    virtual double get_cost() override { return m_ptr_product->get_cost(); }
+    virtual double get_quantity() override { return m_amount; }
+private:
+    size_t m_amount;
+};
+
+class WeightPosition : public Position{
+public:
+    WeightPosition(WeightProduct product, double weight) :
+            Position(product), m_weight(weight) {};
+    virtual double get_cost() override { return m_ptr_product->get_cost(); }
+    virtual double get_quantity() override { return m_weight; }
+private:
+    double m_weight;
+};
